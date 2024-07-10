7bc36db28b7472bbefb8a143522f723cbb0ae723
Makeshkannan659 <makeshkannan659@gmail.com>
2024-07-10 16:04:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var on_button=document.getElementById("on");
+var off_button=document.getElementById("off");
+var image_button=document.getElementById("bulb");
+
+function bulb_on(){
+    image_button.src="./image/bulb_on.jpg"
+}
+
+function bulb_off(){
+    image_button.src="./image/bulb_off.jpg"
+}
